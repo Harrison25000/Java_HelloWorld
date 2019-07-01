6209fdce4c3a8cd24eb5e18ecfc566e8f685d39c
Oliver <ofcharrison@gmail.com>
2019-07-01 15:41:10 +0100

    Console printing arrays.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-            int beerNum = 99;
-            String word = "bottles";
-
-                    while(beerNum > 0){
-
-                        if(beerNum == 1) {
-                            word = "bottle"; // single bottle
-                        }
-
-                        System.out.println(beerNum + " " + word + " of beer on the wall");
-                        System.out.println(beerNum + " " + word + " of beer");
-                        System.out.println("Take one down");
-                        System.out.println("Pass it around");
-                        beerNum -= 1;
-
-                        if (beerNum == 0) {
-                            System.out.println("No more bottles of beer on the wall");
-                        }
-
-        }
+        String[] array = new String[] {"John", "Mary", "Bob"};
+        System.out.println(Arrays.toString(array));
 
     }
 }
