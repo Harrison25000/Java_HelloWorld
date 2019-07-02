fbc640ad61a3b5acb8d32d98a397b7d0d554a16d
Oliver <ofcharrison@gmail.com>
2019-07-02 17:37:35 +0100

    Fixing compiling issues. Array, New CLass, Hobbits.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
-class Books {
-    String title;
-    String author;
-}
+class Hobbits {
 
-class BookTestDrive {
+    String name;
+
     public static void main(String[] args) {
+        Hobbits[] h = new Hobbits[3];
+        int z = 0;
 
-        Books[] myBooks = new Books[3];
-        int x = 0;
-        myBooks[0] = new Books();
-        myBooks[1] = new Books();
-        myBooks[2] = new Books();
-        myBooks[0].title = "The Grapes of Java";
-        myBooks[1].title = "The Java Gatsby";
-        myBooks[2].title = "The Java Coookbook";
-        myBooks[0].author = "bob";
-        myBooks[1].author = "sue";
-        myBooks[2].author = "ian";
+        while (z<3) {
+            h[z] = new Hobbits();
+            h[z].name = "bilbo";
 
-        while (x < 3) {
-            System.out.print(myBooks[x].title);
-            System.out.print(" by ");
-            System.out.println(myBooks[x].author);
-            x += 1;
+            if(z == 1) {
+                h[z].name = "frodo";
+            }
+            if(z == 2) {
+                h[z].name = "sam";
+            }
+
+            System.out.print(h[z].name + " is a ");
+            System.out.println("good Hobbit name");
+            z += 1;
         }
     }
+
+
+
 }
