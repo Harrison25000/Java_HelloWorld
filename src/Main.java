0dbe11ab00a4fbaae3f062835620fcd3abb909b0
Oliver <ofcharrison@gmail.com>
2019-07-03 12:11:52 +0100

    Challenge pg.75, Passing in arguments/parameters into class methods, TDD.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,15 @@
 class Dog {
-    int size;
-    String name;
-
-    void bark() {
-        if (size > 60) {
-            System.out.println("Woof! Woof!");
-        } else if (size > 14) {
-            System.out.println("Ruff! Ruff!");
-        } else {
-            System.out.println("Yip! Yip!");
+    void bark (int numBark) {
+        while (numBark > 0) {
+            System.out.println("Ruff!");
+            numBark -= 1;
         }
     }
 }
 
-class DogTestDrive {
+class DogTest {
     public static void main(String[] args) {
-        Dog one = new Dog();
-        one.size = 65;
-        Dog two = new Dog();
-        two.size = 16;
-        Dog three = new Dog();
-        three.size = 3;
-
-        one.bark();
-        two.bark();
-        three.bark();
+        Dog d = new Dog();
+        d.bark(3);
     }
 }
