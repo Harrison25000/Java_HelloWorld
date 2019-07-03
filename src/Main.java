823f71e01c773a6b0ef9940dcf9559e9a205a2aa
Oliver <ofcharrison@gmail.com>
2019-07-03 15:30:11 +0100

    exercise pg.88, returning values in methods - for methods returning values they cannot be void. Have to set the type of returning property e.g. String, int etc. Whilst defining the method.

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
-class GoodDog {
-    private int size;
+class Clock {
+    String time;
 
-    public int getSize() {
-        return size;
+    void setTime(String t) {
+        time = t;
     }
-
-    public void setSize(int s) {
-        size = s;
-    }
-
-    void bark() {
-        if (size > 60) {
-            System.out.println("Woof!");
-        } else if (size > 14) {
-            System.out.println("Ruff!");
-        } else {
-            System.out.println("Yip!");
-        }
+    String getTime() {
+        return time;
     }
 }
 
-class GoodDogTestDrive {
+class ClockTestDrive {
     public static void main(String[] args) {
-        GoodDog one = new GoodDog();
-        GoodDog two = new GoodDog();
-        one.setSize(70);
-        two.setSize(8);
-        System.out.println("Dog one: " + one.getSize());
-        System.out.println("Dog two: " + two.getSize());
-        one.bark();
-        two.bark();
-        }
+        Clock c = new Clock();
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time: " + tod);
+    }
 }
