14e0aaf4cc5fd9ee0c8c495ea41a9cb797a769a2
Oliver <ofcharrison@gmail.com>
2019-07-05 11:50:30 +0100

    pg. 118: for & if loops.

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,20 @@
-import java.io.*;
+class Output {
+    public static void main(String[] args) {
+        Output o = new Output();
+        o.go();
+    }
 
-class battleship {
-    int[] location;
-    int numOfHits = 0;
-
-    public static void main(String[] args){
-        int userGuesses = 0;
-        GameHelper helper = new GameHelper();
-        battleship dotCom = new battleship();
-        int randomNum = (int) (Math.random() * 5);
-        int[] locations = {randomNum, randomNum+1, randomNum+2};
-        dotCom.setLocationCells(locations);
-        boolean isAlive = true;
-
-        while(isAlive == true) {
-            String guess = helper.getUserInput("enter a number");
-            String result = dotCom.checkYourself(guess);
-            userGuesses += 1;
-            if(result.equals("killed")){
-                isAlive = false;
-                System.out.println("You took "+userGuesses+" guesses");
+    void go() {
+        int y = 7;
+        for(int x=0; x < 8; x++){
+            y++;
+            if(x>4){
+                System.out.print(++y + " ");
             }
-        }
-
-    }
-
-    public void setLocationCells (int[] args) {
-        location = args;
-    }
-
-    public String checkYourself (String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
-        String result = "miss";
-
-        for(int cell : location){
-            if(guess == cell){
-                numOfHits += 1;
-                result = "hit";
+            if (y > 14) {
+                System.out.print("x = " + x);
                 break;
             }
         }
-
-        if(numOfHits == location.length){
-            result = "killed";
-        }
-
-        System.out.println(result);
-        return result;
-    }
-
-}
-
-class GameHelper {
-    public String getUserInput(String prompt) {
-        String inputLine = null;
-        System.out.println(prompt + " ");
-        try {
-            BufferedReader is = new BufferedReader(
-                    new InputStreamReader(System.in));
-            inputLine = is.readLine();
-            if(inputLine.length()==0) return null;}
-        catch (IOException e){
-            System.out.println("IOException: " + e);
-        }
-        return inputLine;
     }
 }
