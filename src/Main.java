21ab224ba6ed3c7f0ea8c3c13d04487b3f0d4825
Oliver <ofcharrison@gmail.com>
2019-07-08 11:56:20 +0100

    pg. 119: for & if loops.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
-class Output {
-    public static void main(String[] args) {
-        Output o = new Output();
-        o.go();
+class MultiFor {
+
+    public static void main(String[] args){
+
+        for(int x = 0; x<4; x++) {
+                for (int y = 4; y > 2; y--) {
+                        System.out.println(x + " " + y);
+
+                }
+                if (x==1) {
+                    x++;
+                }
+            }
+
     }
 
-    void go() {
-        int y = 7;
-        for(int x=0; x < 8; x++){
-            y++;
-            if(x>4){
-                System.out.print(++y + " ");
-            }
-            if (y > 14) {
-                System.out.print("x = " + x);
-                break;
-            }
-        }
-    }
 }
