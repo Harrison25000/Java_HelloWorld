ef7799d380d8b9b60bf0c00ed8a9dbdb560b8b62
Oliver <ofcharrison@gmail.com>
2019-07-09 14:56:18 +0100

    pg. 194: polymorphism, inheritance, Boats, word connnection. *= (seperating files, doesnt require file importation)

--- src/Main.java
+++ src/Main.java
@@ -20,16 +20,3 @@ class Sailboat extends Boat {
         System.out.print("hoist sail ///");
     }
 }
-
-class TestBoats {
-    public static void main(String[] args){
-        Boat b1 = new Boat();
-        Sailboat b2 = new Sailboat();
-        Rowboat b3 = new Rowboat();
-        b2.setLength(32);
-        b1.move();
-        b3.move();
-        b2.move();
-    }
-
-}
