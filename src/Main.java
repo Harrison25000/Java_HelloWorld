5d527f99b0c40abd793800f2f2a6fc1e672ba632
Oliver <ofcharrison@gmail.com>
2019-07-17 17:43:22 +0100

    Randomising numbers, compiling (javac) and running (java ClassName) from terminal, splitting integers, returning ArrayLists

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.*;
-import java.time.*;
-import java.time.format.*;
 import java.util.*;
-import java.util.logging.*;
+import java.util.Random;
 
- class TestClass {
-    public static void main(String[] args) throws IOException{
-        Files.createDirectories(Paths.get("logs"));
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
-        LocalDateTime now = LocalDateTime.now();
-        String date = now.format(df);
-        String logFileName = "logs\\testlog-" + date + ".txt";
-        FileHandler myFileHandler = new FileHandler(logFileName);
-        myFileHandler.setFormatter(new SimpleFormatter());
-        Logger ocajLogger = Logger.getLogger("OCAJ Logger");
-        ocajLogger.setLevel(Level.ALL);
-        ocajLogger.addHandler(myFileHandler);
-        ocajLogger.info("\nThis is a logged information message. ");
-        myFileHandler.close();
-
-    }
+class numbers {
+ public static void main(String[] args) {
+     Random rand = new Random();
+    int n = rand.nextInt(1000);
+     List<Integer> digits = new ArrayList<Integer>();
+     while (n > 0) {
+         digits.add(n%10);
+         n/=10;
+     }
+     System.out.println(Arrays.toString(digits.toArray()));
+ }
 }
