06dec805c43b7c77151ddd73a5298704dd745bb1
Oliver <ofcharrison@gmail.com>
2019-07-22 14:09:17 +0100

    Switch Condition statements. using cases. break clause.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
-class TrueFalse {
+import java.util.Random;
 
-    public static void main(String[] args) {
-        int x = -5;
-        int absoluteValue = x>0 ? x:-x;
-        System.out.println(absoluteValue);
+class SwitchConditionals{
+
+    public static void main(String[] args){
+         SwitchConditionals.generateRandomFish();
     }
 
-}
+    public static String generateRandomFish() {
+        String randomFish;
+        Random randomObject = new Random();
+        int randomNumber = randomObject.nextInt(4);
+        switch(randomNumber) {
+            case 0:
+                randomFish = "Blue Fish";
+                break;
+            case 1:
+                randomFish = "Red Drum";
+                break;
+            case 2:
+                randomFish = "Striped Bass";
+                break;
+            default:
+                randomFish = "Unknown Fish Type";
+                break;
+        }
+        System.out.println(randomFish);
+        return randomFish;
+        }
+    }
