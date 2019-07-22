15fb12716bf5c3f7901b689062371bfb6c173832
Oliver <ofcharrison@gmail.com>
2019-07-22 11:47:48 +0100

    Ternary Operators, pg 58, one line if/else statement,

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 class TrueFalse {
 
     public static void main(String[] args) {
-        boolean b;
-
-        boolean bValue = (b = true);
-
-        if (bValue) System.out.println("TRUE");
-        else System.out.println("FALSE");
-
-        if (bValue = false) System.out.println("TRUE");
-        else System.out.println("FALSE");
-
-        if (bValue == false) System.out.println("TRUE");
-        else System.out.println("FALSE");  //So I think, because bValue is a boolean variable & 'false' is a statement, they are two different objects, thereby returning a value of 'false' which makes the 'if' part of the if/else statement correct, returning "TRUE".
+        int x = -5;
+        int absoluteValue = x>0 ? x:-x;
+        System.out.println(absoluteValue);
     }
 
 }
