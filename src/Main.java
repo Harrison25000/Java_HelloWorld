7694c494a188faf79e0bec0f5b67664d16fea0ca
Oliver <ofcharrison@gmail.com>
2019-07-23 16:46:48 +0100

    Inserting text into String variables.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,9 @@
-import java.util.Random;
+class SwitchConditionals {
 
-class SwitchConditionals{
+    public static void main(String[] args) {
+StringBuilder mateyMessage;
+mateyMessage = new StringBuilder("Shiver Me Timbers");
+System.out.println(mateyMessage.insert(17, " and Bricks!"));
 
-    public static void main(String[] args){
-         SwitchConditionals.generateRandomFish();
     }
-
-    public static String generateRandomFish() {
-        String randomFish;
-        Random randomObject = new Random();
-        int randomNumber = randomObject.nextInt(4);
-        switch(randomNumber) {
-            case 0:
-                randomFish = "Blue Fish";
-                break;
-            case 1:
-                randomFish = "Red Drum";
-                break;
-            case 2:
-                randomFish = "Striped Bass";
-                break;
-            default:
-                randomFish = "Unknown Fish Type";
-                break;
-        }
-        System.out.println(randomFish);
-        return randomFish;
-        }
-    }
+}
