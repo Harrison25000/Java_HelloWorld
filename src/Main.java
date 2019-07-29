a3ede63cadef4a603a1f3306c5f7b729970822b8
Oliver <ofcharrison@gmail.com>
2019-07-29 16:52:35 +0100

    Variable scopes. Instance Variables

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-class SwitchConditionals {
-
-    public static void main(String[] args) {
-StringBuilder mateyMessage;
-mateyMessage = new StringBuilder("Shiver Me Timbers");
-System.out.println(mateyMessage.insert(17, " and Bricks!"));
-
-    }
+class Number
+{
+    int number;
+public Number(int number){
+    this.number = number;
 }
+int getNumber(){
+    return this.number;
+}
+
+void setNumber(int number){
+    this.number = number;
+}
+
+void addThree(Number value){
+    System.out.println("Parameter: Value = " + value.getNumber());
+    value.setNumber(value.getNumber() + 3);
+    System.out.println("Leaving method : value = " + value.getNumber());
+}
+
+public static void main(String[] args) {
+   Number value = new Number(1);
+   System.out.println("Argument: value =   " + value.getNumber());
+   value.addThree(value);
+   System.out.println("After method call = " + value.getNumber());
+}
+
+}
+
