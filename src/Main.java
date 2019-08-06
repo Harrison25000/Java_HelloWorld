259534ef5ec4b175545ef55dc7e4b9b6be79ce67
Oliver <ofcharrison@gmail.com>
2019-08-06 13:48:36 +0100

    access modifiers, pg 299.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 class Main{
-    public static void main(String[] args){
-        ChildClass childClass1 = new ChildClass();
-        ChildClass childClass2 = new ChildClass("test");
+    private static void bob(){
+        System.out.println("bob");
     }
+    public static void runbobrun(){
+        System.out.println("lets run ");
+        Main.bob();
+    }
+}
+
+class Jumanji{
+    public static void main(String[] args){
+        Main.runbobrun();
+    }
+    public void ok(){
+        Main.runbobrun();
+    }
+
 }
 
